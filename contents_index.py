5bc900d166770d776c343e331580c675c799ec93
Suan Kan <suan.kan@cba.com.au>
2019-04-18 16:15:20 +1000

    Code style fixes

--- contents_index.py
+++ contents_index.py
@@ -1,96 +1,103 @@
-class contents_index:
-  '''
-  This class represents "Contents" indices which is described here:
-  https://wiki.debian.org/DebianRepository/Format?action=show&redirect=RepositoryFormat#A.22Contents.22_indices
-  '''
+'''
+This module makes some interface and tooling backend to work with Debian Contents indexes.
+'''
 
-  # This dict will store number of files in each package:
-  # {
-  #   package_0: number_of_files_0,
-  #   package_1: number_of_files_1,
-  #   ...
-  #   package_N: number_of_files_N,
-  # }
-  _packages_size = {}
 
-  # For simplicity we will use just a single debian mirror and just fail if it not available.
-  # TODO: Implement trying iterations over different mirrors.
-  _debian_mirror = 'http://ftp.debian.org/debian/dists/stable/main/'
-
-  def __init__(self, file, arch):
+class ContentsIndex:
     '''
-    This is a constructor for this class
-    '''
-    # TODO why is it needed and what should be here.
-    pass
+    This class represents "Contents" indices which is described here:
+    https://bit.ly/2IDN6pu
 
-  def _curate_line(self, raw_line):
-    '''
-    This method processes a line of "Contents" indices file by:
-    - first space-separate string will be considered as filename
-    - second space-separated string will be considered as comma-separated list of packages.
+    Usage:
+    Instantiating:
+        Set debian mirror URL to use.
+        Set ARCH we dealing with which identifies Contents index file.
+        Get Contents index file
+        Ungzip obtained Contents index file
 
-    Given a raw line:
-    'bin/busybox        utils/busybox,shells/busybox-static'
+    Possible public actions:
+        - calculate number of files in each deb package from
+            Contents index file
+        - get top N packages by number of files
 
-    It will return a list of strings:
-    [
-      'bin/busybox utils/busybox',
-      'bin/busybox shells/busybox-static',
-    ]
-
-    If raw line specifies just one package,
-    'bin/bzcat        utils/bzip2'
-    then it will return a list with a single string:
-    [
-      'bin/bzcat utils/bzip2',
-    ]
-
-    If malformed raw string is passed which does not follow "Contents" indices spec
-    Then result is... I need to think about it.
+    Potential private actions:
+        - constructor
+        - ungzip obtained Contents index file
+        - download Contents index file for a given arch
+        - curate each line of Contents index file
     '''
 
-    pass
+    def __init__(self, arch):
+        '''
+        This is a constructor for this class.
+        '''
+
+    @staticmethod
+    def curate_line(raw_line):
+        '''
+        This method processes a line of "Contents" indices file by:
+        - first space-separate string will be considered as filename
+        - second space-separated string will be considered as
+            comma-separated list of packages.
+
+        Given a raw line:
+        'bin/busybox        utils/busybox,shells/busybox-static'
+
+        It will return a list of strings:
+        [
+        'bin/busybox utils/busybox',
+        'bin/busybox shells/busybox-static',
+        ]
+
+        If raw line specifies just one package,
+        'bin/bzcat        utils/bzip2'
+        then it will return a list with a single string:
+        [
+        'bin/bzcat utils/bzip2',
+        ]
+
+        If malformed raw string is passed which does not follow "Contents" indices spec
+        Then result is... I need to think about it.
+        '''
 
 
-  def _get_contents_file(self, arch):
-    '''
-    This function will download Contents index file from Debian repo for a given architecture.
-    E.g. http://ftp.debian.org/debian/dists/stable/main/Contents-amd64.gz
-    '''
-    pass
+    def _get_contents_file(self):
+        '''
+        This function will download Contents index file
+        from Debian repo for a given architecture.
+        E.g. http://ftp.debian.org/debian/dists/stable/main/Contents-amd64.gz
+        '''
 
-  def _ungz_contents_file(self):
-    '''
-    This function will unarchive gz contents file.
-    '''
+    def _ungzip_contents_file(self):
+        '''
+        This function will unarchive gz contents file.
+        '''
 
-    # import gzip
-    # import shutil
-    # with gzip.open('file.txt.gz', 'rb') as f_in:
-    #   with open('file.txt', 'wb') as f_out:
-    #     shutil.copyfileobj(f_in, f_out)
-    pass
+        # import gzip
+        # import shutil
+        # with gzip.open('file.txt.gz', 'rb') as f_in:
+        #   with open('file.txt', 'wb') as f_out:
+        #     shutil.copyfileobj(f_in, f_out)
 
-  def _get_packages_size(self):
-    '''
-    This function will actually scan the Contents index file and populate self._packages_size dict.
-    '''
+    def get_packages_size(self):
+        '''
+        This function will actually scan the Contents index
+        file and populate self._packages_size dict.
+        '''
 
-    # for package in curr_packages:
-    #   if package not in _packages:
-    #     _packages[package] = 1
-    #   else:
-    #     _packages[package] += 1
-    pass
+        # for package in curr_packages:
+        #   if package not in _packages:
+        #     _packages[package] = 1
+        #   else:
+        #     _packages[package] += 1
 
-  def _get_top(self, n):
-    '''
-    This function will get top n packages from the obtained self._packages_size dict
-    '''
+    def get_top(self, num):
+        '''
+        This function will get top n packages from
+        the obtained self._packages_size dict
+        '''
 
-    # from heapq import nlargest
-    # from collections import Counter
-    # print heapq.nlargest(n, packages.items(), key=lambda i: i[1])
-    # print Counter(packages).most_common(10)
-    pass
+        # from heapq import nlargest
+        # from collections import Counter
+        # print heapq.nlargest(n, packages.items(), key=lambda i: i[1])
+        # print Counter(packages).most_common(10)
